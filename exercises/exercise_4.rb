dc388e41bc174271b58113002235423cd6434a7a
claytonsavage <clytnsavage@gmail.com>
2018-01-16 21:21:45 +0000

    completed exercise 1-6

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,24 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# Borrowing and modifying the code from Exercise one, create 3 more stores:
+
+# Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+
+surrey = Store.create(annual_revenue: 224000, womens_apparel: true, mens_apparel: false, name: 'Surrey')
+whistler = Store.create(annual_revenue: 1900000, womens_apparel: false, mens_apparel: true, name: 'Whistler')
+yaletown = Store.create(annual_revenue: 430000, womens_apparel: true, mens_apparel: true, name: 'Yaletown')
+
+men_stores = Store.where(mens_apparel: true)
+
+men_stores.each do |store|
+  print store.name
+  puts ": $#{ store.annual_revenue }"
+end
+
+women_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+
+women_stores.each do |store|
+  print store.name
+  puts ": $#{ store.annual_revenue }"
+end
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+# Exercise 5: Calculations
+# Output the total revenue for the entire company (all stores), using Active Record's .sum calculation method.
+# On the next line, also output the average annual revenue for all stores.
+# Output the number of stores that are generating $1M or more in annual sales. Hint: Chain together where and size (or count) Active Record methods.
+
+
+sum_stores = Store.sum(:annual_revenue)
+p "The sum of revenue for all stores is #{sum_stores} "
+avg_stores = sum_stores / Store.count
+p "The average revenue per store is #{avg_stores} "
+
+over_a_million = Store.where("annual_revenue > ?", 1000000).count
+p "This many stores make over a million #{over_a_million}"
