45165c25c7b9f5e52a727460f8e9833d63e6a5ab
claytonsavage <clytnsavage@gmail.com>
2018-01-16 22:51:02 +0000

    completed exercise 6

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,19 @@
 class Employee < ActiveRecord::Base
+	belongs_to :store
+	validates :first_name, :last_name, :store, presence: true
+	validate :wage
 
+  def wage
+    if hourly_rate > 200
+      # errors.add(:hourly_rate, “cannot be above 200”)
+    elsif hourly_rate < 40
+      # errors.add(:hourly_rate, “cannot be below 40”)
+    end
+  end
 end
+
+
+# Employees must always have a first name present
+# Employees must always have a last name present
+# Employees have a hourly_rate that is a number (integer) between 40 and 200
+# Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,10 @@
 class Store < ActiveRecord::Base
+	has_many :employees
+	validates :name, :annual_revenue, presence: true, length: { minimum: 3 }
+	validate :income
+ 
+  def income
+    annual_revenue.to_i < 0
+  end
 
 end
